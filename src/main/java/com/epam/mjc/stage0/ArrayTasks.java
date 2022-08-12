357d405cc121cf17169969b210377e8844b64eda
IGAZIEV <islam.gaziev@gmail.com>
2022-08-12 15:39:40 +0500

    Module #5. Task #1. Attempt #1.

--- src/main/java/com/epam/mjc/stage0/ArrayTasks.java
+++ src/main/java/com/epam/mjc/stage0/ArrayTasks.java
@@ -1,5 +1,7 @@
 package com.epam.mjc.stage0;
 
+//imp__ort java.util.Arrays;
+
 /**
  * Here are the tasks for working with the arrays.
  * <p>
@@ -11,6 +13,8 @@ public class ArrayTasks {
      * Return a String[] array that will list all the seasons of the year, starting with winter.
      */
     public String[] seasonsArray() {
+
+        return new String[]{"winter", "spring", "summer","autumn"};
 
     }
 
@@ -25,7 +29,10 @@ public class ArrayTasks {
      * length = 5  -> [1, 2, 3, 4, 5]
      */
     public int[] generateNumbers(int length) {
-
+        int[] result = new int[length];
+        for (int i=0;i<length;i++)
+            result[i] = i+1;
+        return result;
     }
 
     /**
@@ -37,7 +44,9 @@ public class ArrayTasks {
      * arr = [5, -3, -4] -> sum = -2
      */
     public int totalSum(int[] arr) {
-
+        int result=0;
+        for (int j : arr) result = result + j;
+        return result;
     }
 
     /**
@@ -50,7 +59,10 @@ public class ArrayTasks {
      * arr = [5, -3, -4],   number = 10    ->  -1
      */
     public int findIndexOfNumber(int[] arr, int number) {
-
+        for (int i=0;i<arr.length;i++)
+            if (arr[i]==number)
+                return i;
+        return -1;
     }
 
     /**
@@ -63,7 +75,11 @@ public class ArrayTasks {
      * arr = ["pineapple", "apple", "pen"] -> ["pen", "apple", "pineapple"]
      */
     public String[] reverseArray(String[] arr) {
+        String[] reversed = new String[arr.length];
 
+        for (int i=0;i<arr.length;i++)
+            reversed[i] = arr[arr.length-1-i];
+        return reversed;
     }
 
     /**
@@ -78,7 +94,17 @@ public class ArrayTasks {
      * arr = [1, 2]         -> [1, 2]
      */
     public int[] getOnlyPositiveNumbers(int[] arr) {
+        int length = 0, position = 0;
+        for (int k : arr) if (k > 0) length++;
 
+        int[] positives = new int[length];
+        if (length>0)
+            for (int j : arr)
+                if (j > 0) {
+                    positives[position] = j;
+                    position++;
+                }
+        return positives;
     }
 
     /**
@@ -92,6 +118,61 @@ public class ArrayTasks {
      * arr = [[5, 4], [7]]       -> [[7], [4, 5]]
      */
     public int[][] sortRaggedArray(int[][] arr) {
+        /*for (int i : arr[0])
+            System.out.println(i);
+        System.out.println("\nNEXT DIMENSION\n");
+        for (int i : arr[1])
+            System.out.println(i);*/
+        //Sorting array #0
+        if (arr.length>0)
+            for (int i=0;i<arr[0].length;i++) {
+                for (int j = 0; j < arr[0].length; j++) {
+                    int tmp;
+                    if (arr[0][i] < arr[0][j]) {
+                        tmp = arr[0][i];
+                        arr[0][i] = arr[0][j];
+                        arr[0][j] = tmp;
+                    }
+                }
+            }
 
+        //Sorting array #1
+        if (arr.length==2) {
+            for (int i = 0; i < arr[1].length; i++) {
+                for (int j = 0; j < arr[1].length; j++) {
+                    int tmp;
+                    if (arr[1][i] < arr[1][j]) {
+                        tmp = arr[1][i];
+                        arr[1][i] = arr[1][j];
+                        arr[1][j] = tmp;
+                    }
+                }
+            }
+            //Sorting dimensions
+            int[] temp = arr[0];
+            if (arr[0].length > arr[1].length) {
+                arr[0] = arr[1];
+                arr[1] = temp;
+            }
+        }
+        return arr;
+    }
+    public static void main(String[] args){
+        ArrayTasks test = new ArrayTasks();
+        int[][] t = {{10, 5, -10, 2, 5, 7, 1, -88, 30, 91, 3}, {}};//{{10, 5, -10, 2, 5, 7, 0, -88, 0, 91, 3},{}};//
+
+        //System.out.println(t[1][2]);
+
+        //t[0] = new int[] {9, 3, 6, 0, -7, 24, 789032, 911, -93098};
+        //t[1] = new int[] {29, -9, 5, 0};
+        t = test.sortRaggedArray(t);
+        System.out.println(t.length);
+        System.out.println(t[0].length);
+        System.out.println(t[1].length);
+        for (int i : t[0])
+            System.out.println(i);
+        //System.out.println("\nNEXT DIMENSION\n");
+        //for (int i : t[1])
+        //    System.out.println(i);
     }
 }
